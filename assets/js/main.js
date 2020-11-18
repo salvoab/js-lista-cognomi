47f0bebd2c6af22532bf9fee8adc243ff2a5885b
salvoab <salvatore.abate85@gmail.com>
2020-11-18 14:49:21 +0100

    Esercizio completato

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*******************
+* Chiedere all’utente il cognome
+* inserirlo in un array con altri cognomi: "Bianchi’, "Rossi’, "Duzioni’, "Balsano’, "Verdi’
+* stampa la lista ordinata alfabeticamente
+* scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+*********************/
+
+var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+var cognomeRichiesto = prompt("Inserisci il tuo cognome");
+var spanCognomeUtente = document.getElementById("cognome-utente");
+var spanPosizioneCognome = document.getElementById("posizione-cognome");
+var posizioneCognomeRichiesto;
+
+cognomeRichiesto = cognomeRichiesto.toLowerCase();
+cognomeRichiesto = cognomeRichiesto.charAt(0).toUpperCase() + cognomeRichiesto.slice(1);
+listaCognomi.push(cognomeRichiesto);
+listaCognomi.sort();
+
+for(var i=0; i<listaCognomi.length; i++){
+    var cognome = listaCognomi[i];
+    var htmlList = document.getElementById("lista-cognomi").innerHTML;
+    document.getElementById("lista-cognomi").innerHTML = htmlList + "<li>" + cognome + "</li>";
+    if(cognome == cognomeRichiesto){
+        posizioneCognomeRichiesto = i + 1;
+    }
+}
+
+spanCognomeUtente.innerHTML = '"' + cognomeRichiesto + '"';
+spanPosizioneCognome.innerHTML = ": " + posizioneCognomeRichiesto;
